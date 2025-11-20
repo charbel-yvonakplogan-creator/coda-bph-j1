d3a4b598e192ee13cfd34d6828708f8ef6beb4b5
Charbelakplogan2002 <acharbel02@gmail.com>
2025-11-20 13:09:57 +0000

    Ajout de exo3

---
+++ ex3.php
@@ -0,0 +1,13 @@
+<?php
+
+$animal = [
+    "species" => "Chat",
+    "name" => "Curly",
+    "age" => "10 ans"
+];
+
+$species = $animal["species"]; // $species contient Chat
+
+print_r( value: $animal);
+
+?>
