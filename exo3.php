a1cf2d4e7db09a329ddc1d0094038576a364fb5f
Charbelakplogan2002 <acharbel02@gmail.com>
2025-11-20 14:02:03 +0000

    Ajout de exo4

---
+++ exo3.php
@@ -0,0 +1,16 @@
+<?
+
+$users = ["name=>John", "age=>30" 
+ ];
+
+foreach($users as $key => $name)
+{
+    echo "My name is  $key : $name <br>";
+}
+
+foreach($users as $key => $age)
+{
+    echo "My age is $key : $age <br>";
+}
+
+?>
